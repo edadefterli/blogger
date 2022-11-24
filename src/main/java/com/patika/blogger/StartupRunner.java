c3a1a639d32938695812e6de1cc655bbdc6f961e
Eda Defterli <edanur-1999@hotmail.com>
2022-11-24 14:43:57 +0300

    project initialized

---
+++ src/main/java/com/patika/blogger/StartupRunner.java
@@ -0,0 +1,30 @@
+
+package com.patika.blogger;
+ 
+import com.patika.blogger.model.Gender;
+import com.patika.blogger.model.User;
+import com.patika.blogger.repository.ExpenseTypeRepository;
+import com.patika.blogger.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupRunner implements CommandLineRunner{
+    
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ExpenseTypeRepository expenseTypeRepository;
+    
+    @Override
+    public void run(String... args) throws Exception {
+        
+        User newUser = new User(2, "Kürşat", "Ufuk", "k@gmail.com", Gender.MALE,null , null);
+        userRepository.save(newUser);
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //ExpenseType newExpenseType = new ExpenseType(1,"test2",EnableFlag.NO);
+        //expenseTypeRepository.save(newExpenseType);
+    }
+    
+}
